d28d7cfd0c231a282477e79f5ce62fe77d42e32e
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-10-29 21:08:00 +0100

    FileWriter and BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,24 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
     public static void main(String[] args) {
-        String path = "C:\\in.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line = br.readLine();
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
 
-            while (line != null){
-                System.out.println(line);
-                line = br.readLine();
+        String path = "c:\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
             }
         }
         catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Erro: " + e.getMessage());
         }
-
     }
 }
